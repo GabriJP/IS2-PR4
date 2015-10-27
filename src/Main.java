73cbb11dd30c625084aaa88b81fab9be6b561475
Gabriel <gabriel.jimenez102@alu.ulpgc.es>
2015-10-27 09:30:49 +0000

    Recursivo

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,28 @@ import java.io.File;
  */
 public class Main {
     public static void main(String[] args) {
-        File files = new File("./root");
-        String[] names = files.list();
-        for(String name : names){
-            System.out.println(name);
+        File file = new File("./root");
+        print(file.listFiles(), "");
+    }
+
+    private static void print(File[] files, String indent){
+        if(files != null){
+            for(File file : files){
+                if(file.isDirectory()){
+                    if(file.isHidden()){
+                        System.out.println(indent + "<<" + file.getName() + "/" + ">>");
+                    } else {
+                        System.out.println(indent + file.getName() + "/");
+                    }
+                    print(file.listFiles(), indent + "\t");
+                } else {
+                    if(file.isHidden()){
+                        System.out.println(indent + "<<" + file.getName() + ">>");
+                    } else {
+                        System.out.println(indent + file.getName());
+                    }
+                }
+            }
         }
     }
 }
